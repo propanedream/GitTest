32b93fab72b27222e727e212a1bad9ecc85376c6
propanedream <eaaadey@students.eaaa.dk>
2022-09-15 11:03:21 +0200

    opdatering

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,5 +3,7 @@ package test;
 public class Test {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(29293);
+        System.out.println("skdjfklsdjflksjdflksjdlkfj");
     }
 }
